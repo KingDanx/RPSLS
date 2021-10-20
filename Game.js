d657d305fbf4fb3c6e9e5521ef93d3c4a981ed49
Dan Prudhomme <dannyprud@yahoo.com>
2021-10-19 22:51:45 -0500

    change many functions to accept a parameter that allows for either a player or an ai to be placed into the program to play against player1.  edit AI.js getPlayerChoice function to output something comparerator can read.

--- Game.js
+++ Game.js
@@ -21,13 +21,13 @@ class Game{
             case "1":
                 this.playerNumber = 2;
                 this.determinePlayers(this.player1.name, this.player2.name);
-                this.roundAnnounce(this.player1, this.player2);
+                this.roundAnnounce(this.player2);
                 this.main();
             break;
             case "2":
                 this.playerNumber = 1;
                 this.determinePlayers(this.player1.name, this.ai.name);
-                this.roundAnnounce(this.player1, this.ai);
+                this.roundAnnounce(this.ai);
                 this.main();
             break;
         
@@ -42,12 +42,12 @@ class Game{
         if(this.playerNumber === 1){
             this.player1.getPlayerChoice();
             this.ai.getPlayerChoice();
-            this.gameMechanics();
+            this.gameMechanics(this.ai);
         }
         else{
             this.player1.getPlayerChoice();
             this.player2.getPlayerChoice();
-            this.gameMechanics();
+            this.gameMechanics(this.player2);
         }
      }
 
@@ -63,59 +63,60 @@ class Game{
         }
     }
 
-    gameMechanics(){
-        if(this.player1.gesture === this.player2.gesture){
+    gameMechanics(otherPlayer){
+        if(this.player1.gesture === otherPlayer.gesture){
                 console.log(`You both choose ${this.player1.gesture}, play again.`);
                 return this.main();
         }
-        this.comparerator("paper", "rock", "\nPaper covers Rock");
-        this.comparerator(`scissors`, `paper`, "\nScissors cuts Paper");
-        this.comparerator(`rock`, `scissors`, "\nRock crushes Scissors");
-        this.comparerator(`rock`, `lizard`, "\nRock crushes Lizard");
-        this.comparerator(`lizard`, `spock`, "\nLizard poisons Spock");
-        this.comparerator(`spock`, `scissors`, "\nSpock smashes Scissors");
-        this.comparerator(`scissors`, `lizard`, "\nScissors decapitates Lizard");
-        this.comparerator(`lizard`, `paper`, "\nLizard eats Paper");
-        this.comparerator(`paper`, `spock`, "\nPaper disproves Spock");
-        this.comparerator(`spock`, `rock`, "\nSpock vaporizes Rock");
+        this.comparerator("paper", "rock", "\nPaper covers Rock", otherPlayer);
+        this.comparerator(`scissors`, `paper`, "\nScissors cuts Paper", otherPlayer);
+        this.comparerator(`rock`, `scissors`, "\nRock crushes Scissors", otherPlayer);
+        this.comparerator(`rock`, `lizard`, "\nRock crushes Lizard", otherPlayer);
+        this.comparerator(`lizard`, `spock`, "\nLizard poisons Spock", otherPlayer);
+        this.comparerator(`spock`, `scissors`, "\nSpock smashes Scissors", otherPlayer);
+        this.comparerator(`scissors`, `lizard`, "\nScissors decapitates Lizard", otherPlayer);
+        this.comparerator(`lizard`, `paper`, "\nLizard eats Paper", otherPlayer);
+        this.comparerator(`paper`, `spock`, "\nPaper disproves Spock", otherPlayer);
+        this.comparerator(`spock`, `rock`, "\nSpock vaporizes Rock", otherPlayer);
 
-        this.roundCheck();
+        this.roundCheck(otherPlayer);
     }
 
-    comparerator(winner, looser, string){
-        if((this.player1.gesture == winner || this.player1.gesture == looser) && (this.player2.gesture == winner || this.player2.gesture == looser)){
+    comparerator(winner, looser, string, otherPlayer){
+        if((this.player1.gesture == winner || this.player1.gesture == looser) && (otherPlayer.gesture == winner || otherPlayer.gesture == looser)){
             console.log(string);
             if(this.player1.gesture == winner){
                 this.player1.points++;
                 console.log(`${this.player1.name} scores a point and has ${this.player1.points}\n`);
             }
-            else if(this.player2.gesture == winner){
-                this.player2.points++;
-                console.log(`${this.player2.name} scores a point and has ${this.player2.points}\n`);
+            else if(otherPlayer.gesture == winner){
+                otherPlayer.points++;
+                console.log(`${otherPlayer.name} scores a point and has ${otherPlayer.points}\n`);
             }
         }
     }
 
-    roundCheck(){
-        if(this.roundAnnounce() === true){
+    roundCheck(otherPlayer){
+        if(this.roundAnnounce(otherPlayer) === true){
             this.main();
         }
         else{
-            if(this.player1.points > this.player2.points){
+            if(this.player1.points > otherPlayer.points){
                 console.log(`${this.player1.name} is the winner!\n`);
                 this.playAgain()
                 
             }
             else{
-                console.log(`${this.player2.name} is the winner!\n`);
+                console.log(`${otherPlayer.name} is the winner!\n`);
                 this.playAgain()
                 
             }
         }  
     }
-    roundAnnounce(){
-        if((this.player1.points / this.rounds) < 0.5 && (this.player2.points / this.rounds) < 0.5){
-            console.log(`Round ${this.roundCounter} of ${this.rounds}:\n\n${this.player1.name}'s Points: ${this.player1.points}\n${this.player2.name}'s Points: ${this.player2.points}\n`);
+    
+    roundAnnounce(otherPlayer){
+        if((this.player1.points / this.rounds) < 0.5 && (otherPlayer.points / this.rounds) < 0.5){
+            console.log(`Round ${this.roundCounter} of ${this.rounds}:\n\n${this.player1.name}'s Points: ${this.player1.points}\n${otherPlayer.name}'s Points: ${otherPlayer.points}\n`);
             this.roundCounter++
             return true;
         }
@@ -126,6 +127,10 @@ class Game{
         let userInput = prompt();
         userInput = userInput.trim().toLowerCase();
         if(userInput === "yes"){
+            this.player1.points = 0;
+            this.player2.points = 0;
+            this.ai.points = 0;
+            this.roundCounter = 1;
             this.player1.name = "Player 1";
             this.player2.name = "Player 2";
             this.newGame();
